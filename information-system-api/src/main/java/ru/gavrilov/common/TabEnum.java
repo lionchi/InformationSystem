03a299e2bef633a44d08efdfa34a83efde6571a1
GavrilovIvan <ivan.gavrilov@informicus.ru>
2018-03-24 16:52:02 +0300

    add method getActiveTab in MainController.java

---
+++ information-system-api/src/main/java/ru/gavrilov/common/TabEnum.java
@@ -0,0 +1,28 @@
+package ru.gavrilov.common;
+
+public enum TabEnum {
+    CUMPUTER_SYSTEM("Computer System"),
+    FILE_SYSTEM("File System"),
+    CPU("CPU"),
+    MEMORY("Memory"),
+    HARD_DISKS("HardDisks"),
+    USB_DEVICES("USB Devices"),
+    NETWORK("Network"),
+    SENSORS("Sensors"),
+    DISPLAY("Display");
+
+    private String nameTab;
+
+    TabEnum(String nameTab) {
+        this.nameTab = nameTab;
+    }
+
+    public String getNameTab() {
+        return nameTab;
+    }
+
+    @Override
+    public String toString() {
+        return nameTab;
+    }
+}
--- information-system-api/src/main/java/ru/gavrilov/controllers/MainController.java
+++ information-system-api/src/main/java/ru/gavrilov/controllers/MainController.java
@@ -9,9 +9,15 @@ import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import ru.gavrilov.common.Controller;
+import ru.gavrilov.common.TabEnum;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 public class MainController implements Controller {
     private Stage stage;
+    private List<TabEnum> tabEnumList = Arrays.asList(TabEnum.values());
 
     @FXML
     private TabPane tabPane;
@@ -29,6 +35,43 @@ public class MainController implements Controller {
     }
 
     private Tab getActiveTab(){
+        Tab activeTab = tabPane.getSelectionModel().getSelectedItem();
+        Optional<TabEnum> tabEnum = tabEnumList.stream()
+                .filter(tabEnum1 -> tabEnum1.getNameTab().equals(activeTab.getText()))
+                .findFirst();
+        if(tabEnum.isPresent()){
+            switch(tabEnum.get()){
+                case CUMPUTER_SYSTEM:
+                    //TODO
+                    break;
+                case FILE_SYSTEM:
+                    //TODO
+                    break;
+                case CPU:
+                    //TODO
+                    break;
+                case MEMORY:
+                    //TODO
+                    break;
+                case HARD_DISKS:
+                    //TODO
+                    break;
+                case USB_DEVICES:
+                    //TODO
+                    break;
+                case NETWORK:
+                    //TODO
+                    break;
+                case SENSORS:
+                    //TODO
+                    break;
+                case DISPLAY:
+                    //TODO
+                    break;
+                default:
+                    break;
+            }
+        }
         return null;
     }
 
